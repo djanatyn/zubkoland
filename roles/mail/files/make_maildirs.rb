41cfd898682e8cef98994d3909178483e673fd76
Jonathan Strickland <djanatyn@gmail.com>
2015-03-27 22:53:04 +0000

    update mail config

--- roles/mail/files/make_maildirs.rb
+++ roles/mail/files/make_maildirs.rb
@@ -7,7 +7,7 @@ abort "must be run as root!" unless runner == 'root'
 abort "usage: #{$0} <user>" unless ARGV.length == 1
 user = ARGV[0]
 
-mail_directory = "/home/#{user}/mail/"
+mail_directory = "/home/#{user}/Mail/"
 
 if File.exists? mail_directory
   puts "#{mail_directory} already exists!"
