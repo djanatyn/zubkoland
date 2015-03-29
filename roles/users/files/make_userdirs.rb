6c821cc45e256698153ece473b1d0684afcfd668
Jonathan Strickland <djanatyn@gmail.com>
2015-03-29 08:21:40 +0000

    refactor directory creation into single script

---
+++ roles/users/files/make_userdirs.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+
+require 'fileutils'
+
+# this shouldn't be run by normal users
+runner=%x{whoami}.chomp
+abort "must be run as root!" unless runner == 'root'
+
+abort "usage: #{$0} <user>" unless ARGV.length == 1
+user = ARGV[0]
+
+mail_directory = "/home/#{user}/Mail/"
+
+unless File.exists? mail_directory
+  %x{/usr/bin/maildirmake #{mail_directory}}
+  File.chown user, user, dir
+end
+
+# make regular directories here
+directories = %W{public_html}
+directories.map! {|d| "/home/#{user}/#{d}"}
+
+directories.each do |dir|
+  unless File.exists? dir
+    FileUtils.mkdir dir, :mode => 0755
+    FileUtils.chown user, user, dir
+  end
+end
